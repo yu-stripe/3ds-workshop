8c057708d405f3f3fb3f1baae8180e7d720349ae
Yu Kitazume <yu@stripe.com>
2024-11-21 09:31:19 +0900

    add admin pge

--- public/main.js
+++ public/main.js
@@ -1,3 +1,2 @@
 const stripe = Stripe("pk_test_51NQQoIEzgtKktpOy7rP0KtN9n9zBguSzEPYRvjaGuDzcFek2N9gN69tikNBWtdb2Pq2FkkHYWks0qydQBcJEpu3i00SbjW9HWP");
-//const backend = "https://threeds-workshop.onrender.com";
-const backend = "http://localhost:4242";
+const backend = "https://threeds-workshop.onrender.com";
