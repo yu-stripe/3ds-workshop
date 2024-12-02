eb30a37ce99a38e0fac61750aba5857c3587b704
Yu Kitazume <yu@stripe.com>
2024-12-03 07:55:40 +0900

    try to add csp hearder

--- server.rb
+++ server.rb
@@ -10,6 +10,7 @@ before do
   response.headers['Access-Control-Allow-Origin'] = '*'
   response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
   response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  response.headers['Content-Security-Policy'] = "sandbox allow-same-origin"
 end
 
 options "*" do
